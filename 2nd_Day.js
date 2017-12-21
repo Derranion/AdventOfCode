6f8b1729606621571fc44698e4a4ae11dbf94133
Shellby <shellby666@gmail.com>
2017-12-21 03:50:57 +0300

    AdventOfCode\feature: 2nd day 2nd part added

--- 2nd_Day.js
+++ 2nd_Day.js
@@ -22,10 +22,38 @@ arrays.reduce((acc,curr)=>acc+=maxMinDifference(curr),0)
 
 function maxMinDifference(inputArray){
     return inputArray.reduce((acc, curr, index, currArr)=>{
-            console.log('Acc: '+acc+' Curr: '+curr)
             if( +curr < acc[0] || acc[0] === 0 ) acc[0] = +curr
             if( +curr > acc[1] ) acc[1] = +curr
-            if( index === currArr.length - 1 ) { console.log('acc[1]: '+acc[1]+' acc[0]'+acc[0]); return acc[1] - acc[0] }
+            if( index === currArr.length - 1 ) { return acc[1] - acc[0] }
             return acc
     },[0,0])
+}
+
+//day 2 part 2
+/*
+the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+In the second row, the two numbers are 9 and 3; the result is 3.
+In the third row, the result is 2.
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+What is the sum of each row's result in your puzzle input?
+*/
+
+function evenlyDivisible(inputArray){
+    return inputArray.reduce((acc, curr, index, array)=>{
+        curr = +curr;
+        return array.slice(index+1).reduce((acc2, curr2, index2, array2)=>{
+            curr2 = +curr2;
+            if( curr % curr2 === 0 ) return curr / curr2
+            if( curr2 % curr === 0 ) return curr2 / curr
+            return acc2
+        },0) + acc
+    },0)
 }
